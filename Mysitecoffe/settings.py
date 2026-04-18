6ee600b37baa062120da52d5789e8bf5fb753b26
NoelSandoval-15 <sandoval.erick@ficct.uagrm.edu.bo>
2026-04-18 00:38:29 -0400

    Initial commit: Proyecto Cafeteria - Repositorio personal

--- Mysitecoffe/settings.py
+++ Mysitecoffe/settings.py
@@ -89,12 +89,12 @@ WSGI_APPLICATION = 'Mysitecoffe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.environ.get('DB_NAME', BASE_DIR / 'db.sqlite3'),
-        'USER': os.environ.get('DB_USER', ''),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': os.environ.get('DB_HOST', ''),
-        'PORT': os.environ.get('DB_PORT', ''),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cafeteria',
+        'USER': 'postgres.wjfmbtmwjgxocnyqguis',
+        'PASSWORD':'220036314Ensm.',
+        'HOST': 'aws-1-us-east-1.pooler.supabase.com',
+        'PORT': 6543,
     }
 }
 
